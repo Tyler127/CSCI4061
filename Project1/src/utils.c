912190d62185297f32f1064fc4254e1bce0d0ee9
JacksonReichow <146782097+JacksonReichow@users.noreply.github.com>
2023-10-04 20:20:01 -0500

    Update utils.c

--- Project1/src/utils.c
+++ Project1/src/utils.c
@@ -2,9 +2,61 @@
 
 // Create N files and distribute the data from the input file evenly among them
 // See section 3.1 of the project writeup for important implementation details
+// Hint: Use fseek() and ftell() to determine the size of the file
+
 void partition_file_data(char *input_file, int n, char *blocks_folder) {
-    // Hint: Use fseek() and ftell() to determine the size of the file
+    FILE *fp;
+    size_t block_size, remaining_bytes, i;
+    
+    printf("%s\n", input_file);
+
+
+    // Open the input file
+    fp = fopen(input_file, "r");
+    if (!fp) {
+        perror("Failed to open the input file");
+        exit(1);
+    }
+
+    // Determine the size of the file
+    fseek(fp, 0, SEEK_END);
+    size_t file_size = ftell(fp);
+    rewind(fp);
+
+    // Calculate the size of each block
+    block_size = file_size / n;
+    remaining_bytes = file_size % n;
+
+    // Create N blocks and write data to each block
+    for (i = 0; i < n; i++) {
+        int current_block_size;
+        if(i == (n-1)){
+            current_block_size = block_size + remaining_bytes;
+        } else {
+            current_block_size = block_size;
+        }
+
+        printf("%s\n", blocks_folder);
+
+        char file_path[100];
+        sprintf(file_path, "%s/%d.txt", blocks_folder, i+1);
+        printf("%s\n", file_path);
+
+        FILE *new_file = fopen(file_path, "a");
+
+        char buffer[1000];
+        fread(buffer, 1, current_block_size, fp);
+        // fprint("%s\n", &buffer);
+
+        fprintf(new_file, "%s", buffer);
+
+
+    }
+
+    // Close the input file
+    fclose(fp);
 }
+
 
 
 // ##### DO NOT MODIFY THIS FUNCTION #####
